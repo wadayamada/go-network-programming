22d828275644afb3a0f51fc08a91b1e0c6d06984
wada <wada0421514@gmail.com>
2025-02-23 13:07:54 +0900

    v6のicmp受信ロジックは一旦削除

--- ip/receiver/icmpReceiver.go
+++ ip/receiver/icmpReceiver.go
@@ -32,18 +32,5 @@ func main() {
 				}
 			}
 		}
-
-		// IPv6 の場合
-		ipv6Layer := packet.Layer(layers.LayerTypeIPv6)
-		if ipv6Layer != nil {
-			ipv6, _ := ipv6Layer.(*layers.IPv6)
-			if ipv6.NextHeader == layers.IPProtocolICMPv6 {
-				icmpv6Layer := packet.Layer(layers.LayerTypeICMPv6)
-				if icmpv6Layer != nil {
-					icmpv6, _ := icmpv6Layer.(*layers.ICMPv6)
-					fmt.Printf("Received ICMPv6 packet: Type=%d, Code=%d\n", icmpv6.TypeCode.Type(), icmpv6.TypeCode.Code())
-				}
-			}
-		}
 	}
 }
